ab4d45e2120dab8a7af8b813ed742dffaf5d99e5
Pepe Cano <825430+ppcano@users.noreply.github.com>
2023-12-05 15:56:32 +0100

    Update dependencies and test for latest k6 version

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import { Rate } from 'k6/metrics';
 
 import { generateSubscriber } from './generators/subscriber';
 
-const baseUrl = 'https://httpbin.test.loadimpact.com/anything';
+const baseUrl = 'https://k6-http.grafana.fun/anything';
 const urls = {
   form: `${baseUrl}/form`,
   submit: `${baseUrl}/form/subscribe`,
 };
 
-const formFailRate = new Rate('failed form fetches');
-const submitFailRate = new Rate('failed form submits');
+const formFailRate = new Rate('failed_form_fetches');
+const submitFailRate = new Rate('failed_form_submits');
 
 export const options = {
   vus: 300,
   duration: '10s',
   thresholds: {
-    'failed form submits': ['rate<0.1'],
-    'failed form fetches': ['rate<0.1'],
+    'failed_form_fetches': ['rate<0.1'],
+    'failed_form_submits': ['rate<0.1'],
     'http_req_duration': ['p(95)<400']
   }
 };
